ebb87e55aa6d82c8ed09387fb0f7408d161445fd
Lenovo <katashirina89@gmail.com>
2023-09-27 19:18:07 +0300

    Implemented first task

--- main.py
+++ main.py
@@ -1,3 +1,25 @@
 #print("Homework3 Banul")
 
-Користувач вводить із клавіатури номер дня тижня (1-7). Необхідно вивести на екран назви дня тижня.
+#1.Користувач вводить із клавіатури номер дня тижня (1-7). Необхідно вивести на екран назви дня тижня.
+
+try:
+    day = int(input())
+    if day == 1:
+        print("Monday")
+    elif day == 2:
+        print("Tuesday")
+    elif day == 3:
+        print("Wednesday")
+    elif day == 4:
+        print("Thursday")
+    elif day == 5:
+        print("Friday")
+    elif day == 6:
+        print("Saturday")
+    elif day == 7:
+        print("Sunday")
+    else:
+        print("You entered incorrect data")
+
+except ValueError as error:
+    print("You entered incorrect data")
