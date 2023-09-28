e7bf4e0558ec0141bba90ff77c5bcf8ce0b319fe
Lenovo <katashirina89@gmail.com>
2023-09-28 23:55:23 +0300

    Implemented third task

--- main.py
+++ main.py
@@ -23,23 +23,47 @@
 
 #except ValueError as error:
     #print("You entered incorrect data")
-
+################################################
 #2. Користувач вводить два числа. Визначити, чи рівні ці числа, і, якщо ні, вивести їх на екран у порядку зростання
 
+#try:
+    #num1 = int(input("Enter first number: "))
+    #num2 = int(input("Enter second number "))
+
+    #if num1!= num2 and num1 < num2:
+
+        #print(str(num1) + " " + str(num2))
+
+   # elif num1 != num2 and num1 > num2:
+
+        #print(str(num2) + " " + str(num1))
+
+   # else:
+
+        #print("num1 = num2")
+#finally:
+    #print("End of calculation")
+    ###########################################
+
+#3.Користувач вводить два числа та матем дію: + - * або / Залежно від введеної матем дії вивести результат
+
 try:
+
     num1 = int(input("Enter first number: "))
-    num2 = int(input("Enter second number "))
+    num2 = int(input("Enter second  number: "))
+    user_select = str(input("Enter match action: "))
+    math_action = ("Enter math action")
+    match math_action:
+        case '+':
+            print("num1 + num2")
+        case '-':
+            print("num1 - num2")
+        case '*':
+            print("num1 * num2")
+        case '/':
+            print("num1 / num2")
+        case _:
+            print("you entered something wrong")
+except Exception as e:
+    print(f"Error: {e}")
 
-    if num1!= num2 and num1 < num2:
-
-        print(str(num1) + " " + str(num2))
-
-    elif num1 != num2 and num1 > num2:
-
-        print(str(num2) + " " + str(num1))
-
-    else:
-
-        print("num1 = num2")
-finally:
-    print("End of calculation")
